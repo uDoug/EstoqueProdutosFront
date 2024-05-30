9cfb57481e90b5eac1ebd3d5e680196da0fa7620
Douglas <douglasdamascena13@gmail.com>
2024-05-29 23:30:45 -0300

    layout finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Card } from './components/card/card';
+import { SearchBar } from './components/searchBar/searchBar';
 import { useProductData } from './hooks/useProductData';
+
 //import { ProductData } from './interface/ProductData';
 
 
@@ -9,10 +11,37 @@ function App() {
 
   return (
     <div className='app'>
-      <h1>Estoque</h1>  
-      <div className="card-grid">
-        {data?.map(productData => <Card id={productData.id} nome={productData.nome} descricao={productData.descricao} quantidade={productData.quantidade} />)}
-      </div>  
+      <h1>Estoque</h1> 
+      <div className='divAdd'>
+        <button className='btnAdicionar' title='Adicionar novo'> + </button>
+      </div> 
+
+      <div className="container">
+        <div className="top">
+          <div className="searchBar">
+            <SearchBar /> 
+          </div>
+        </div>
+
+        <div className="titles">
+            <div id="nome">
+              <h5>Nome</h5>
+            </div>    
+            <div id="descricao">
+              <h5 >Descrição</h5>
+            </div>
+            <div id="quantidade">
+              <h5>Quantidade</h5>
+            </div>
+          
+          
+        </div>
+          
+        <div className="card-grid">
+          {data?.map(productData => <Card id={productData.id} nome={productData.nome} descricao={productData.descricao} quantidade={productData.quantidade} />)}
+        </div> 
+      </div>
+      
     </div>
   )
 }
---
+++ src/components/searchBar/searchBar.tsx
@@ -0,0 +1,14 @@
+import './searchBar.css'
+
+
+export function SearchBar(){
+
+    return (
+        <div className="divSearchBar">
+            <input type="search" name="searchBar" className="search" placeholder="Pesquisar..." />
+
+        </div>
+
+    )
+
+}
